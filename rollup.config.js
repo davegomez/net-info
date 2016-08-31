d32af13494ac9997c4e21cb181d1d7e9e2956f99
David Gomez <dg.dave@gmail.com>
2016-08-31 13:43:27 -0400

    Install Rollup dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import babel from 'rollup-plugin-babel'
+import async from 'rollup-plugin-async'
+import ramda from 'rollup-plugin-ramda'
 
 export default {
     entry: 'src/index.js',
     format: 'cjs',
-    plugins: [ babel() ],
+    plugins: [ ramda(), async(), babel() ],
     sourceMap: true,
     targets: [
         { dest: 'dist/net-info.js', format: 'cjs' },
